7da9737e73c50d507d719d29df5a275ed522e98e
CheFu <kurisanimaluleke77@gmail.com>
2026-08-26 16:10:15 +0200

    Refactor code structure for improved readability and maintainability

--- android/app/src/main/java/co/za/chefu/infinity/MainActivity.kt
+++ android/app/src/main/java/co/za/chefu/infinity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.kurisani.infinity2048
+package co.za.chefu.infinity
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/co/za/chefu/infinity/MainApplication.kt
+++ android/app/src/main/java/co/za/chefu/infinity/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.kurisani.infinity2048
+package co.za.chefu.infinity
 
 import android.app.Application
 import android.content.res.Configuration
